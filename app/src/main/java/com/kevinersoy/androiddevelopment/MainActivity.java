0ae1d5df9fdef536975990cd452e2284cecfa82e
Kevin Ersoy <kevinersoy@kevinersoy.com>
2018-03-15 12:58:35 -0700

    Added layout files for fragments, created fragment classes, implemented fragment swapping via button press.  Making use of fragment callback from Selector fragment to MainActivity.

--- app/src/main/java/com/kevinersoy/androiddevelopment/MainActivity.java
+++ app/src/main/java/com/kevinersoy/androiddevelopment/MainActivity.java
@@ -1,13 +1,85 @@
 package com.kevinersoy.androiddevelopment;
 
+import android.app.FragmentContainer;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.net.Uri;
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    implements Selector.OnFragmentSelectorListener, Location.OnFragmentLocationListener,
+    ListItems.OnFragmentListItemsListener {
+
+    public static final String LOCATION_BUTTON = "com.kevinersoy.androiddevelopment.LOCATION_BUTTON";
+    public static final String LIST_ITEMS_BUTTON = "com.kevinersoy.androiddevelopment.LIST_ITEMS_BUTTON";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        //set fragment left
+        FrameLayout layoutLeft = findViewById(R.id.fragment_left);
+        ft.replace(layoutLeft.getId(), new Selector());
+
+        //deal with fragment right present or not
+        FrameLayout layoutRight = findViewById(R.id.fragment_right);
+        if(layoutRight != null){
+            ft.replace(layoutRight.getId(), new Location());
+        }
+        ft.commit();
+
+
+    }
+
+    @Override
+    public void onFragmentSelectorInteraction(String string) {
+        switch (string) {
+            case LOCATION_BUTTON:
+                //set location fragment in right if landscape or left if portrait
+                if(findViewById(R.id.fragment_right) != null)
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_right, new Location())
+                            .commit();
+                else
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_left, new Location())
+                            .addToBackStack(null)
+                            .commit();
+                break;
+            case LIST_ITEMS_BUTTON:
+                //set location fragment in right if landscape or left if portrait
+                if(findViewById(R.id.fragment_right) != null)
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_right, new ListItems())
+                            .commit();
+                else
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_left, new ListItems())
+                            .addToBackStack(null)
+                            .commit();
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown button pressed" + string);
+        }
+
+    }
+
+    @Override
+    public void onFragmentLocationInteraction(Uri uri) {
+        //Not using this functionality yet
+    }
+
+    @Override
+    public void onFragmentListItemsInteraction(String string) {
+        //Not using this functionality yet
     }
 }
